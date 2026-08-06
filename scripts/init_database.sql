31f409981220f29586b13d2d59cb8d23164b08c5
Bartek <153931393+rikiadonis@users.noreply.github.com>
2026-08-06 08:25:14 +0200

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,41 @@
+/*
+=================================================================================
+Create Database and Schemas
+================================================================================-
+Script Purpose:
+  This script creates a new databases named 'DrillDataWarehouse' after checking if it already exists.
+  Id database exists, it is dropped and recreated. Additionaly, the scripts sets up three schemas
+within databse: 'bronze', 'silver', and 'gold'.
 
+WARNING:
+  Running this script will drop the entire 'DrillDataWarehouse' database if it exists.
+  All data in database will be pernamently deleted. Proceed with caution 
+  and ensure you have proper backups before running this scripts.
+*/
+
+USE master;
+GO
+
+-- Drop and reaceated the 'DrillDataWarehouse'
+IF EXISTS (SELECT 1 FROM sys.database WHERE name = 'DrillDataWarehouse')
+BEGIN
+  ALTER DATABASE DrillDataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+  DROP DATABASE DrillDataWarehouse;
+END;
+GO
+
+-- Create the 'DrillDataWarehouse' database\
+CREATE DATABASE DrillDataWarehouse;
+GO
+
+USE DrillDataWarehouse;
+GO
+
+-- Create Schemas
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
