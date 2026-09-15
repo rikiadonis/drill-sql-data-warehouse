1b2146fe4302ded05a329f291a3f5ad8f0c279ba
Bartek <153931393+rikiadonis@users.noreply.github.com>
2026-09-15 13:08:04 +0200

    Fix typo in comment about truncating bronze tables

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -5,7 +5,7 @@ Stored Procedure: Load Bronze Layer (Source -> Bronze)
 Script Purpose:
   This stored procedure loads data into the 'bronze' schema from external CSV files.
   It performs the following action.
-  - Truncates the bronze tables before leading data.
+  - Truncates the bronze tables before loading data.
   - Uses the 'BULK INSERT' command to load data from CSV files to bronze tables.
 
 Parameters:
